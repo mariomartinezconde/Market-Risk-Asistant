5e2d0546cde5b0705d058d57bb5315947c7aee76
mariomartinezconde <mario.martinez.conde@msspain.com>
2026-03-20 22:41:30 +0100

    Delete app directory

--- app/__init__.py
+++
@@ -1,4 +0,0 @@
-from .models import QueryRequest, QueryResponse, HealthResponse
-from .orchestrator import answer_query
-
-__all__ = ["QueryRequest", "QueryResponse", "HealthResponse", "answer_query"]
